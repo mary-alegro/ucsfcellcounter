f4ca4416809a6e8f933ca2921e23ffb1f7ef40c6
Maryana Alegro <maryana@Aldebaran-2.local>
2016-04-21 09:55:37 -0700

    ...

---
+++ matlab/compute_stats.m
@@ -0,0 +1,116 @@
+function labels = compute_stats(img_orig,mask_seg,csv)
+
+%
+% IMG_ORIG: original image
+% MASK_SEG: segmentation mask with classes
+% CSV: photoshop CSV
+%
+
+
+[r c N] = size(img_orig);
+mask2 = logical(mask_seg);
+mask2 = imresize(mask2,[r c]);
+
+center = regionprops(mask2,'Centroid');
+nCells = length(center);
+
+center = cat(1,center.Centroid);
+
+idx = find(csv(:,1) == 0); %all
+pts_a = round(csv(idx,2:3));
+a_set = sub2ind([r c],pts_a(:,2),pts_a(:,1));
+
+idx = find(csv(:,1) == 1); %red
+pts_r = round(csv(idx,2:3));
+r_set = sub2ind([r c],pts_r(:,2),pts_r(:,1));
+
+idx = find(csv(:,1) == 2); %green
+pts_g = round(csv(idx,2:3));
+g_set = sub2ind([r c],pts_g(:,2),pts_g(:,1));
+
+idx = find(csv(:,1) == 3); %yellow
+pts_y = round(csv(idx,2:3));
+y_set = sub2ind([r c],pts_y(:,2),pts_y(:,1));
+
+[labels nL] = bwlabel(mask2);
+
+fprintf('Number of red: %d\n',length(r_set));
+fprintf('Number of green: %d\n',length(g_set));
+fprintf('Number of yellow: %d\n',length(y_set));
+fprintf('\nNumber of segmented cells: %d\n',nL);
+
+se = strel('disk',15);
+
+mask3 = zeros(r,c);
+
+%compute true and false positives
+TP = zeros(nL,1);
+FP = zeros(nL,1);
+for l=1:nL   
+    m1 = logical(zeros(r,c));
+    idx = find(labels == l);
+    m1(idx) = 1;
+    
+    m2 = imdilate(m1,se);
+    idx_set = find(m2 == 1);
+    
+    mask3(m2 == 1) = 1;
+    
+    %Ua = intersect(idx_set,a_set);
+    Ur = intersect(idx_set,r_set);
+    Ug = intersect(idx_set,g_set);
+    Uy = intersect(idx_set,y_set);
+    
+    if ~isempty(Ur) || ~isempty(Ug) || ~isempty(Uy)
+        TP(l) = 1;
+    else
+        FP(l) = 1;
+    end
+    
+    %fprintf('\nCell %d\n',l);
+    %fprintf('all: %d | red: %d | green: %d | yellow: %d\n',length(Ua),length(Ur),length(Ug),length(Uy));
+    %fprintf('red: %d | green: %d | yellow: %d\n',length(Ur),length(Ug),length(Uy));
+end
+
+%compute false negatives
+m_set = find(mask3 == 1);
+
+nP = length(g_set);
+FN_g = zeros(nP,1);
+for p=1:nP
+    u = intersect(g_set(p),m_set);
+    if isempty(u)
+        FN_g(p) = 1;
+    end
+end
+
+nP = length(r_set);
+FN_r = zeros(nP,1);
+for p=1:nP
+    u = intersect(r_set(p),m_set);
+    if isempty(u)
+        FN_r(p) = 1;
+    end
+end
+
+nP = length(y_set);
+FN_y = zeros(nP,1);
+for p=1:nP
+    u = intersect(y_set(p),m_set);
+    if isempty(u)
+        FN_y(p) = 1;
+    end
+end
+
+fprintf('True positives: %d\n',sum(TP));
+fprintf('False positives: %d\n',sum(FP));
+fprintf('False negatives: %d\n',sum(FN_r) + sum(FN_g));
+fprintf('    R: %d\n',sum(FN_r));
+fprintf('    G: %d\n',sum(FN_g));
+fprintf('    Y: %d\n',sum(FN_y));
+
+
+
+
+
+
--- matlab/print_points_csv.m
+++ matlab/print_points_csv.m
@@ -1,47 +1,19 @@
-function print_points_csv(img,csv)
-
-
-
-
+function [pts_b, pts_r, pts_g, pts_y] = print_points_csv(img,csv)
 
 
 idx = find(csv(:,1) == 0);
-pts_b = csv(idx,:);
+pts_b = csv(idx,2:3);
 idx = find(csv(:,1) == 1);
-pts_r = csv(idx,:);
+pts_r = csv(idx,2:3);
 idx = find(csv(:,1) == 2);
-pts_g = csv(idx,:);
+pts_g = csv(idx,2:3);
 idx = find(csv(:,1) == 3);
-pts_y = csv(idx,:);
+pts_y = csv(idx,2:3);
 
-imshow(img); hold on,
-[r c N] = size(pts_b);
-for f=1:r
-    x = csv(f,2);
-    y = csv(f,3);
-    plot(x,y,'b*');
-end
+imshow(img); 
+hold on
+%plot(pts_b(:,1),pts_b(:,2),'w*','MarkerSize', 10);
+plot(pts_r(:,1),pts_r(:,2),'r*','MarkerSize', 10);
+plot(pts_g(:,1),pts_g(:,2),'g*','MarkerSize', 10);
+plot(pts_y(:,1),pts_y(:,2),'y*','MarkerSize', 10);
 
-figure, imshow(img); hold on,
-[r c N] = size(pts_r);
-for f=1:r
-    x = csv(f,2);
-    y = csv(f,3);
-    plot(x,y,'r*');
-end
-
-figure, imshow(img); hold on,
-[r c N] = size(pts_g);
-for f=1:r
-    x = csv(f,2);
-    y = csv(f,3);
-    plot(x,y,'g*');
-end
-
-figure, imshow(img); hold on,
-[r c N] = size(pts_y);
-for f=1:r
-    x = csv(f,2);
-    y = csv(f,3);
-    plot(x,y,'y*');
-end
